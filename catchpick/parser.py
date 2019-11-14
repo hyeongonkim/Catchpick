455fd7cb3c1c1c481ddd59c38e89f80206a0a266
Song-Junho <star054321@naver.com>
2019-11-14 14:43:19 +0900

    파서 수정중
    파싱된 검색어를 가공하는 작업중입니다.

--- catchpick/parser.py
+++ catchpick/parser.py
@@ -6,17 +6,33 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "catchpick.settings")
 import django
 django.setup()
 # HTTP GET Request
-from newsCatch.models import TitleData
+from newsCatch.models import TitleData, AccumulateData, NewsTestData
 
 
+def now_parse():
+    TitleData.objects.all().delete()
+    resp = requests.get('https://www.naver.com/')
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    titles = soup.select('.ah_list .ah_k')
+    return titles;
 
 
-resp = requests.get('https://www.naver.com/')
-soup = BeautifulSoup(resp.text, 'html.parser')
-titles = soup.select('.ah_list .ah_k')
+def accumulate_data():
+    for i in range(1,21):
+        for j in range(1,21):
 
-for title in titles:
-    TitleData(title=title.get_text(),time= time.time()).save()
+    
+    return 0;
+
+
+if __name__=='__main__':
+    titles = now_parse()
+    cnt = 1
+    # for title in titles:
+    #     TitleData(title=title.get_text(),time= time.time(),nowRank=cnt).save()
+    #     cnt+=1
+    print(type(titles))
+
 
 
 
