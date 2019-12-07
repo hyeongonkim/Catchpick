8b03b3a7c9325b4457ded73f30f1c0490352b851
hyeongonkim <simonkim@kookmin.ac.kr>
2019-12-08 02:31:49 +0900

    충돌시 예외처리

--- catchpick/chknews.py
+++ catchpick/chknews.py
@@ -11,20 +11,6 @@ django.setup()
 from newsCatch.models import NewsTestData
 from newsCatch.models import VerifiedData
 
-driver_path = os.path.join(os.getcwd() + '/chromedriver')
-# driver_path = os.path.join('/Users/simonkim/PycharmProjects/KTISparse/catchpick/macchromedriver')
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('window-size=1920x1080')
-options.add_argument("disable-gpu")
-options.add_argument('no-sandbox')
-options.add_argument(
-    "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
-driver = webdriver.Chrome(driver_path, chrome_options=options)
-
-driver.get('https://search.naver.com/search.naver?where=news&sm=tab_jum&query=')
-driver.find_element_by_id("_search_option_btn").click()
-
 
 def determineCategory(localCategory):
     ourCategory = ['정치', '경제', '사회', '국제', '스포츠', '문화']
@@ -144,111 +130,31 @@ def getNews(company, keyword):
     return [title1, link1, time1]
 
 
-companys = ['ca_1032', 'ca_1023', 'ca_1025', 'ca_1028', 'ca_1056', 'ca_1214', 'ca_1055',
-            'ca_1052']  # 경향, 조선, 중앙, 한겨레, KBS, MBC, SBS, YTN
+driver_path = os.path.join(os.getcwd() + '/chromedriver')
+# driver_path = os.path.join('/Users/simonkim/PycharmProjects/KTISparse/catchpick/macchromedriver')
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+options.add_argument('no-sandbox')
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+driver = webdriver.Chrome(driver_path, chrome_options=options)
 
-nowTime = time.time()
-#기존데이터 리프레시
-for i in VerifiedData.objects.all():
-    title = i.title
-    if nowTime - float(i.time) >= 604800:
-        i.delete()
-        continue
-    news = []
-    newsCnt = 0
-    companyCnt = 0
-    for j in companys:
-        nowData = getNews(j, title)
-        if '분 전' not in nowData[2] and '시간 전' not in nowData[2]:
-            companyCnt = companyCnt + 1
-            continue
-        testList = title.split()
-        chkTitleTest = False
-        for tempkey in testList:
-            if tempkey in nowData[0]:
-                chkTitleTest = True
-        if not chkTitleTest:
-            companyCnt = companyCnt + 1
-            continue
-        if companyCnt == 0:
-            nowData.append('경향')
-        elif companyCnt == 1:
-            nowData.append('조선')
-        elif companyCnt == 2:
-            nowData.append('중앙')
-        elif companyCnt == 3:
-            nowData.append('한겨레')
-        elif companyCnt == 4:
-            nowData.append('KBS')
-        elif companyCnt == 5:
-            nowData.append('MBC')
-        elif companyCnt == 6:
-            nowData.append('SBS')
-        elif companyCnt == 7:
-            nowData.append('YTN')
-        news.append(nowData)
-        newsCnt = newsCnt + 1
-        companyCnt = companyCnt + 1
+try:
+    driver.get('https://search.naver.com/search.naver?where=news&sm=tab_jum&query=')
+    driver.find_element_by_id("_search_option_btn").click()
 
-    verify = [i.news_KH_title, i.news_KH_link, i.news_HKR_title, i.news_HKR_link, i.news_CS_title, i.news_CS_link,
-              i.news_CA_title, i.news_CA_link, i.news_KBS_title, i.news_KBS_link, i.news_MBC_title, i.news_MBC_link,
-              i.news_SBS_title, i.news_SBS_link, i.news_YTN_title, i.news_YTN_link]
-    for k in news:
-        if '경향' == k[3]:
-            verify[0] = k[0]
-            verify[1] = k[1]
-        elif '조선' == k[3]:
-            verify[4] = k[0]
-            verify[5] = k[1]
-        elif '중앙' == k[3]:
-            verify[6] = k[0]
-            verify[7] = k[1]
-        elif '한겨레' == k[3]:
-            verify[2] = k[0]
-            verify[3] = k[1]
-        elif 'SBS' == k[3]:
-            verify[12] = k[0]
-            verify[13] = k[1]
-        elif 'YTN' == k[3]:
-            verify[14] = k[0]
-            verify[15] = k[1]
-        elif 'MBC' == k[3]:
-            verify[10] = k[0]
-            verify[11] = k[1]
-        elif 'KBS' == k[3]:
-            verify[8] = k[0]
-            verify[9] = k[1]
-    tempTitle = i.title
-    tempTime = i.time
-    tempMaxRank = i.maxRank
-    tempCategory = i.category
-    i.delete()
-    VerifiedData(title=tempTitle, time=tempTime, maxRank=tempMaxRank, category=tempCategory,
-                 news_KH_title=verify[0],
-                 news_KH_link=verify[1], news_HKR_title=verify[2], news_HKR_link=verify[3], news_CS_title=verify[4],
-                 news_CS_link=verify[5], news_CA_title=verify[6], news_CA_link=verify[7], news_KBS_title=verify[8],
-                 news_KBS_link=verify[9], news_MBC_title=verify[10], news_MBC_link=verify[11],
-                 news_SBS_title=verify[12], news_SBS_link=verify[13], news_YTN_title=verify[14],
-                 news_YTN_link=verify[15]).save()
 
-# 신규데이터 검사
-for i in NewsTestData.objects.all():
-    title = i.title
-    try:
-        rankChange = VerifiedData.objects.get(title=title)
-        rankChange.maxRank = i.maxRank
-        rankChange.save()
-    except VerifiedData.DoesNotExist:
-        testAlready = title.split()
-        chkTestAlready = False
-        for splited in testAlready:
-            try:
-                test = VerifiedData.objects.get(title=splited)
-                chkTestAlready = True
-                break
-            except VerifiedData.DoesNotExist:
-                continue
-        if chkTestAlready:
+    companys = ['ca_1032', 'ca_1023', 'ca_1025', 'ca_1028', 'ca_1056', 'ca_1214', 'ca_1055',
+                'ca_1052']  # 경향, 조선, 중앙, 한겨레, KBS, MBC, SBS, YTN
+
+    nowTime = time.time()
+    #기존데이터 리프레시
+    for i in VerifiedData.objects.all():
+        title = i.title
+        if nowTime - float(i.time) >= 604800:
+            i.delete()
             continue
         news = []
         newsCnt = 0
@@ -285,51 +191,149 @@ for i in NewsTestData.objects.all():
             news.append(nowData)
             newsCnt = newsCnt + 1
             companyCnt = companyCnt + 1
-        if newsCnt >= 3:
-            category = []
-            verify = []
-            for cnt in range(16):
-                verify.append('empty')
-            for k in news:
-                if '경향' == k[3]:
-                    category.append(KHcategory(k[1]))
-                    verify[0] = k[0]
-                    verify[1] = k[1]
-                elif '조선' == k[3]:
-                    category.append(CScategory(k[1]))
-                    verify[4] = k[0]
-                    verify[5] = k[1]
-                elif '중앙' == k[3]:
-                    category.append(CAcategory(k[1]))
-                    verify[6] = k[0]
-                    verify[7] = k[1]
-                elif '한겨레' == k[3]:
-                    category.append(HKRcategory(k[1]))
-                    verify[2] = k[0]
-                    verify[3] = k[1]
-                elif 'SBS' == k[3]:
-                    category.append(SBScategory(k[1]))
-                    verify[12] = k[0]
-                    verify[13] = k[1]
-                elif 'YTN' == k[3]:
-                    category.append(YTNcategory(k[1]))
-                    verify[14] = k[0]
-                    verify[15] = k[1]
-                elif 'MBC' == k[3]:
-                    verify[10] = k[0]
-                    verify[11] = k[1]
-                elif 'KBS' == k[3]:
-                    verify[8] = k[0]
-                    verify[9] = k[1]
-            cnt = Counter(category)
-            mostCategory = cnt.most_common(1)[0][0]
-            VerifiedData(title=i.title, time=i.time, maxRank=i.maxRank, category=mostCategory, news_KH_title=verify[0],
-                         news_KH_link=verify[1], news_HKR_title=verify[2], news_HKR_link=verify[3],
-                         news_CS_title=verify[4],
-                         news_CS_link=verify[5], news_CA_title=verify[6], news_CA_link=verify[7],
-                         news_KBS_title=verify[8],
-                         news_KBS_link=verify[9], news_MBC_title=verify[10], news_MBC_link=verify[11],
-                         news_SBS_title=verify[12], news_SBS_link=verify[13], news_YTN_title=verify[14],
-                         news_YTN_link=verify[15]).save()
-    i.delete()
+
+        verify = [i.news_KH_title, i.news_KH_link, i.news_HKR_title, i.news_HKR_link, i.news_CS_title, i.news_CS_link,
+                i.news_CA_title, i.news_CA_link, i.news_KBS_title, i.news_KBS_link, i.news_MBC_title, i.news_MBC_link,
+                i.news_SBS_title, i.news_SBS_link, i.news_YTN_title, i.news_YTN_link]
+        for k in news:
+            if '경향' == k[3]:
+                verify[0] = k[0]
+                verify[1] = k[1]
+            elif '조선' == k[3]:
+                verify[4] = k[0]
+                verify[5] = k[1]
+            elif '중앙' == k[3]:
+                verify[6] = k[0]
+                verify[7] = k[1]
+            elif '한겨레' == k[3]:
+                verify[2] = k[0]
+                verify[3] = k[1]
+            elif 'SBS' == k[3]:
+                verify[12] = k[0]
+                verify[13] = k[1]
+            elif 'YTN' == k[3]:
+                verify[14] = k[0]
+                verify[15] = k[1]
+            elif 'MBC' == k[3]:
+                verify[10] = k[0]
+                verify[11] = k[1]
+            elif 'KBS' == k[3]:
+                verify[8] = k[0]
+                verify[9] = k[1]
+        tempTitle = i.title
+        tempTime = i.time
+        tempMaxRank = i.maxRank
+        tempCategory = i.category
+        i.delete()
+        VerifiedData(title=tempTitle, time=tempTime, maxRank=tempMaxRank, category=tempCategory,
+                    news_KH_title=verify[0],
+                    news_KH_link=verify[1], news_HKR_title=verify[2], news_HKR_link=verify[3], news_CS_title=verify[4],
+                    news_CS_link=verify[5], news_CA_title=verify[6], news_CA_link=verify[7], news_KBS_title=verify[8],
+                    news_KBS_link=verify[9], news_MBC_title=verify[10], news_MBC_link=verify[11],
+                    news_SBS_title=verify[12], news_SBS_link=verify[13], news_YTN_title=verify[14],
+                    news_YTN_link=verify[15]).save()
+
+    # 신규데이터 검사
+    for i in NewsTestData.objects.all():
+        title = i.title
+        try:
+            rankChange = VerifiedData.objects.get(title=title)
+            rankChange.maxRank = i.maxRank
+            rankChange.save()
+        except VerifiedData.DoesNotExist:
+            testAlready = title.split()
+            chkTestAlready = False
+            for splited in testAlready:
+                try:
+                    test = VerifiedData.objects.get(title=splited)
+                    chkTestAlready = True
+                    break
+                except VerifiedData.DoesNotExist:
+                    continue
+            if chkTestAlready:
+                continue
+            news = []
+            newsCnt = 0
+            companyCnt = 0
+            for j in companys:
+                nowData = getNews(j, title)
+                if '분 전' not in nowData[2] and '시간 전' not in nowData[2]:
+                    companyCnt = companyCnt + 1
+                    continue
+                testList = title.split()
+                chkTitleTest = False
+                for tempkey in testList:
+                    if tempkey in nowData[0]:
+                        chkTitleTest = True
+                if not chkTitleTest:
+                    companyCnt = companyCnt + 1
+                    continue
+                if companyCnt == 0:
+                    nowData.append('경향')
+                elif companyCnt == 1:
+                    nowData.append('조선')
+                elif companyCnt == 2:
+                    nowData.append('중앙')
+                elif companyCnt == 3:
+                    nowData.append('한겨레')
+                elif companyCnt == 4:
+                    nowData.append('KBS')
+                elif companyCnt == 5:
+                    nowData.append('MBC')
+                elif companyCnt == 6:
+                    nowData.append('SBS')
+                elif companyCnt == 7:
+                    nowData.append('YTN')
+                news.append(nowData)
+                newsCnt = newsCnt + 1
+                companyCnt = companyCnt + 1
+            if newsCnt >= 3:
+                category = []
+                verify = []
+                for cnt in range(16):
+                    verify.append('empty')
+                for k in news:
+                    if '경향' == k[3]:
+                        category.append(KHcategory(k[1]))
+                        verify[0] = k[0]
+                        verify[1] = k[1]
+                    elif '조선' == k[3]:
+                        category.append(CScategory(k[1]))
+                        verify[4] = k[0]
+                        verify[5] = k[1]
+                    elif '중앙' == k[3]:
+                        category.append(CAcategory(k[1]))
+                        verify[6] = k[0]
+                        verify[7] = k[1]
+                    elif '한겨레' == k[3]:
+                        category.append(HKRcategory(k[1]))
+                        verify[2] = k[0]
+                        verify[3] = k[1]
+                    elif 'SBS' == k[3]:
+                        category.append(SBScategory(k[1]))
+                        verify[12] = k[0]
+                        verify[13] = k[1]
+                    elif 'YTN' == k[3]:
+                        category.append(YTNcategory(k[1]))
+                        verify[14] = k[0]
+                        verify[15] = k[1]
+                    elif 'MBC' == k[3]:
+                        verify[10] = k[0]
+                        verify[11] = k[1]
+                    elif 'KBS' == k[3]:
+                        verify[8] = k[0]
+                        verify[9] = k[1]
+                cnt = Counter(category)
+                mostCategory = cnt.most_common(1)[0][0]
+                VerifiedData(title=i.title, time=i.time, maxRank=i.maxRank, category=mostCategory, news_KH_title=verify[0],
+                            news_KH_link=verify[1], news_HKR_title=verify[2], news_HKR_link=verify[3],
+                            news_CS_title=verify[4],
+                            news_CS_link=verify[5], news_CA_title=verify[6], news_CA_link=verify[7],
+                            news_KBS_title=verify[8],
+                            news_KBS_link=verify[9], news_MBC_title=verify[10], news_MBC_link=verify[11],
+                            news_SBS_title=verify[12], news_SBS_link=verify[13], news_YTN_title=verify[14],
+                            news_YTN_link=verify[15]).save()
+        i.delete()
+except:
+    driver.quit()
 driver.quit()
